c011299e3ae9e6f1736e27b197aa68f25711e2fc
mlaane <goobric@gmail.com>
2023-09-21 19:58:30 +0800

    chk room with specific key exists

---
+++ functions/RoomExists.go
@@ -0,0 +1,11 @@
+package lemin
+
+// Checks the adding Room for existency
+func RoomExists(s []*Room, key string) bool {
+	for _, v := range s {
+		if v.Key == key {
+			return true
+		}
+	}
+	return false
+}
