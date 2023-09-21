9d7048528d3ca3e3f3966ddd8cb997d8fd0211ac
mlaane <goobric@gmail.com>
2023-09-21 19:33:53 +0800

    chk for unique room coordinates

---
+++ functions/IsValidCoords.go
@@ -0,0 +1,12 @@
+package lemin
+
+//read current room coordinates, and check against coords of following rooms
+// IsValidCoords checks if the room coordinates are valid and unique
+func IsValidCoords(nodeCoord []string, data *Input) bool {
+	for _, coord := range data.Coords {
+		if coord[0] == nodeCoord[0] && coord[1] == nodeCoord[1] { 
+			return false //duplicate coords, not unique
+		}
+	}
+	return true
+}
