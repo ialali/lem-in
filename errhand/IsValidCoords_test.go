3a1c3406b9b135fd954a4cc309d46ec3a111fa50
mlaane <goobric@gmail.com>
2023-09-23 00:14:38 +0800

    linked pkg lemin to IsValidCoords test

--- errhand/IsValidCoords_test.go
+++ errhand/IsValidCoords_test.go
@@ -2,22 +2,24 @@ package lemin
 
 import (
 	"testing"
+
+	lemin "lemin/functions"
 )
 
 func TestIsValidCoords(t *testing.T) {
     // Create a sample Input struct
-    data := &Input{}
+    data := &lemin.Input{}
 
     // Test case 1: Valid coordinates
     validCoords := []string{"2", "3"}
-    isValid := IsValidCoords(validCoords, data)
+    isValid := lemin.IsValidCoords(validCoords, data)
     if !isValid {
         t.Error("Expected coordinates to be valid, but got invalid.")
     }
 
     // Test case 2: Negative coordinates
     negativeCoords := []string{"-1", "-1"}
-    isValid = IsValidCoords(negativeCoords, data)
+    isValid = lemin.IsValidCoords(negativeCoords, data)
     if isValid {
         t.Error("Expected coordinates to be invalid (negative), but got valid.")
     }
@@ -25,7 +27,7 @@ func TestIsValidCoords(t *testing.T) {
     // Test case 3: Duplicate coordinates
     data.Coords = [][]string{{"2", "3"}}
     duplicateCoords := []string{"2", "3"}
-    isValid = IsValidCoords(duplicateCoords, data)
+    isValid = lemin.IsValidCoords(duplicateCoords, data)
     if isValid {
         t.Error("Expected coordinates to be invalid (duplicate), but got valid.")
     }
