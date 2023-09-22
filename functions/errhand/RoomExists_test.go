a40ad63babb9022a7b89251d31b3c1db1e0ebd26
mlaane <goobric@gmail.com>
2023-09-22 19:47:55 +0800

    Room test

---
+++ functions/errhand/RoomExists_test.go
@@ -0,0 +1,26 @@
+package lemin
+
+import (
+	"testing"
+)
+
+func TestRoomExists(t *testing.T) {
+	// Create a sample slice of rooms
+	rooms := []*Room{
+		&Room{Key: "Room1"},
+		&Room{Key: "Room2"},
+		&Room{Key: "Room3"},
+	}
+
+	// Test case 1: Check for an existing room
+	exists := RoomExists(rooms, "Room2")
+	if !exists {
+		t.Error("Expected to find an existing room, but got false.")
+	}
+
+	// Test case 2: Check for a non-existing room
+	exists = RoomExists(rooms, "Room4")
+	if exists {
+		t.Error("Expected not to find a non-existing room, but got true.")
+	}
+}
