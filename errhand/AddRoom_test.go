28d7c3f0e0e0b4e0813ba676c453403a721049b1
mlaane <goobric@gmail.com>
2023-09-22 23:57:12 +0800

    linked pkg lemin to AddRoom test

--- errhand/AddRoom_test.go
+++ errhand/AddRoom_test.go
@@ -1,31 +1,32 @@
 package lemin
 
 import (
+	lemin "lemin/functions"
 	"testing"
 )
 
 func TestAddRoom(t *testing.T) {
     // Create a sample graph
-    graph := &Graph{
-        Rooms: []*Room{
+    graph := &lemin.Graph{
+        Rooms: []*lemin.Room{
             {Key: "Room1"},
             {Key: "Room2"},
         },
     }
 
     // Test case 1: Add a valid room
-    err := AddRoom(graph, "Room3")
+    err := lemin.AddRoom(graph, "Room3")
     if err != nil {
         t.Errorf("Expected no error, but got an error: %v", err)
     }
 
     // Verify that the room was added correctly
-    if !RoomExists(graph.Rooms, "Room3") {
+    if !lemin.RoomExists(graph.Rooms, "Room3") {
         t.Error("Expected Room3 to be in the graph's Rooms, but it's not.")
     }
 
     // Test case 2: Attempt to add an existing room
-    err = AddRoom(graph, "Room1")
+    err = lemin.AddRoom(graph, "Room1")
     if err == nil {
         t.Error("Expected an error for an existing room, but got no error.")
     }
