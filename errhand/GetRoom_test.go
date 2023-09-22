4d5b399f636ae22cd003bf0f13e354cdadeba4f9
mlaane <goobric@gmail.com>
2023-09-23 00:09:12 +0800

    linked pkg lemin to GetRoom test

--- errhand/GetRoom_test.go
+++ errhand/GetRoom_test.go
@@ -1,21 +1,22 @@
 package lemin
 
 import (
+	lemin "lemin/functions"
 	"testing"
 )
 
 func TestGetRoom(t *testing.T) {
     // Create a sample Graph with rooms
-    graph := &Graph{
-        Rooms: []*Room{
-            &Room{Key: "Room1"},
-            &Room{Key: "Room2"},
-            &Room{Key: "Room3"},
+    graph := &lemin.Graph{
+        Rooms: []*lemin.Room{
+            &lemin.Room{Key: "Room1"},
+            &lemin.Room{Key: "Room2"},
+            &lemin.Room{Key: "Room3"},
         },
     }
 
     // Test case 1: Get an existing room
-    room1 := GetRoom(graph, "Room1")
+    room1 := lemin.GetRoom(graph, "Room1")
     if room1 == nil {
         t.Error("Expected to get an existing room, but got nil.")
     }
@@ -24,7 +25,7 @@ func TestGetRoom(t *testing.T) {
     }
 
     // Test case 2: Get a non-existing room
-    room4 := GetRoom(graph, "Room4")
+    room4 := lemin.GetRoom(graph, "Room4")
     if room4 != nil {
         t.Error("Expected to get nil for a non-existing room, but got a room.")
     }
