3009bd1a4898626fe3337142dbebfd7ba222fab7
mlaane <goobric@gmail.com>
2023-09-23 00:41:21 +0800

    linked pkg lemin to RoomExists test

--- errhand/RoomExists_test.go
+++ errhand/RoomExists_test.go
@@ -1,27 +1,26 @@
 package lemin
 
 import (
-	"testing"
 	lemin "lemin/functions"
+	"testing"
 )
 
-
-	// Test case 1: Check for an existing roomfunc TestRoomExists(t *testing.T) {
+func TestRoomExists(t *testing.T) {
 	// Create a sample slice of rooms
-	rooms := []*Room{
-		&Room{Key: "Room1"},
-		&Room{Key: "Room2"},
-		&Room{Key: "Room3"},
+	rooms := []*lemin.Room{
+		&lemin.Room{Key: "Room1"},
+		&lemin.Room{Key: "Room2"},
+		&lemin.Room{Key: "Room3"},
 	}
 
 	// Test case 1: Check for an existing room
-	exists := RoomExists(rooms, "Room2")
+	exists := lemin.RoomExists(rooms, "Room2")
 	if !exists {
 		t.Error("Expected to find an existing room, but got false.")
 	}
 
 	// Test case 2: Check for a non-existing room
-	exists = RoomExists(rooms, "Room4")
+	exists = lemin.RoomExists(rooms, "Room4")
 	if exists {
 		t.Error("Expected not to find a non-existing room, but got true.")
 	}
