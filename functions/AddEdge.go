3e38c369bb4fb02204b662b00ef4574f5d11412a
mlaane <goobric@gmail.com>
2023-09-22 16:40:50 +0800

    changed variables from to -> fromRm toRm for clarity

--- functions/AddEdge.go
+++ functions/AddEdge.go
@@ -3,21 +3,19 @@ package lemin
 import "fmt"
 
 // Adds an edge to the graph
-// from = fromRm
-// to = toRm
-// Children = Connection
+// Children = Relationship Connection to Parent Node
 
-func AddEdge(graph *Graph, from, to string) error {
+func AddEdge(graph *Graph, fromRm, toRm string) error {
 	// get Room
-	fromRoom := GetRoom(graph, from)
-	toRoom := GetRoom(graph, to)
+	fromRoom := GetRoom(graph, fromRm)
+	toRoom := GetRoom(graph, toRm)
 
 	// error handling: current room, next room don't exist; from-to Room are same 
 	if fromRoom == nil || toRoom == nil || fromRoom == toRoom {
-		err := fmt.Errorf("ERR: invalid edge(%v --> %v)", from, to)
+		err := fmt.Errorf("ERR: invalid edge(%v --> %v)", fromRm, toRm)
 		return err
-	} else if RoomExists(fromRoom.Children, to) { //does edge already exist?
-		err := fmt.Errorf("ERR: existing edge(%v --> %v)", from, to)
+	} else if RoomExists(fromRoom.Children, toRm) { //does edge already exist?
+		err := fmt.Errorf("ERR: existing edge(%v --> %v)", fromRm, toRm)
 		return err
 	} else {
 		// add the edge (connection) between the rooms (nodes)
