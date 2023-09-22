7f708bcdf5110d4920de2ed3038b8d40d963d625
mlaane <goobric@gmail.com>
2023-09-23 00:22:43 +0800

    linked pkg lemin to IsValidRoom test

--- errhand/IsValidRoom_test.go
+++ errhand/IsValidRoom_test.go
@@ -1,40 +1,41 @@
 package lemin
 
 import (
+	lemin "lemin/functions"
 	"testing"
 )
 
 func TestIsValidRoom(t *testing.T) {
     // Create a sample Input struct
-    data := &Input{}
+    data := &lemin.Input{}
 
     // Test case 1: Valid room with coordinates
-    err := IsValidRoom("Room1 2 3", "", data)
+    err := lemin.IsValidRoom("Room1 2 3", "", data)
     if err != nil {
         t.Errorf("Expected no error, but got an error: %v", err)
     }
 
     // Test case 2: Room with invalid format
-    err = IsValidRoom("InvalidRoomFormat", "", data)
+    err = lemin.IsValidRoom("InvalidRoomFormat", "", data)
     if err == nil {
         t.Error("Expected an error for invalid room format, but got no error.")
     }
 
     // Test case 3: Room with negative coordinates
-    err = IsValidRoom("Room2 -1 -1", "", data)
+    err = lemin.IsValidRoom("Room2 -1 -1", "", data)
     if err == nil {
         t.Error("Expected an error for negative coordinates, but got no error.")
     }
 
     // Test case 4: Room with duplicate coordinates
     data.Coords = [][]string{{"2", "3"}}
-    err = IsValidRoom("Room3 2 3", "", data)
+    err = lemin.IsValidRoom("Room3 2 3", "", data)
     if err == nil {
         t.Error("Expected an error for duplicate coordinates, but got no error.")
     }
 
     // Test case 5: Room with reserved starting character
-    err = IsValidRoom("#InvalidRoom 1 2", "", data)
+    err = lemin.IsValidRoom("#InvalidRoom 1 2", "", data)
     if err == nil {
         t.Error("Expected an error for reserved starting character, but got no error.")
     }
